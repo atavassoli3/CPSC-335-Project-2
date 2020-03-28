58bc0fa9d4c4866352369c89384a09addd7dfac0
Vincent DeAugustine <32307897+vdeaugustine@users.noreply.github.com>
2020-03-28 16:49:48 -0700

    Merge Sort Working

--- Script1.js
+++ Script1.js
@@ -2,22 +2,13 @@
 
 var mergeHolder = [];
 var arr = ['0', 5, 'A', 6, 2, 7, 'B', 2, 'B' , 6, 0, 3];
+
+//convert all the values to strings to make sorting work properly 
+for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].toString();
+}
 //QuickSort(arr);
 //mergeSort(arr);
-
-
-// was using this to see if javascript passes arrays as reference
-// console.log("arr before testing func: " , arr.toString());
-// function testing (parr){
-
-//     for (var i = 0; i < parr.length; i++){
-//         parr[i] = 0;
-//     }
-// }
-// testing(arr);
-// console.log("arr after testing func: " , arr.toString());
-
-console.log("Array before sort " , arr.toString());
 
 function printArray(A) 
 { 
@@ -36,9 +27,14 @@ function merge (arr, l, m, r) {
     var n1 = m - l + 1;
     var n2 = r - m;
 
+
+    /* create temp arrays */
     var L = [];
     var R = [];
 
+
+
+/* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++) {
         L[i] = arr[l + i];
     }
@@ -56,6 +52,8 @@ function merge (arr, l, m, r) {
 
     console.log(mergeHolder);
 
+
+    /* Merge the temp arrays back into arr[l..r]*/
     i = 0; // Initial index of first subarray 
     j = 0; // Initial index of second subarray 
     k = l; // Initial index of merged subarray 
@@ -114,13 +112,15 @@ function mergeSort(arr, l, r)
         mergeSort(arr, m+1, r); 
         // printArray(arr, array_Size);
         merge(arr, l, m, r); 
+        
 
     } 
+    
 } 
 
 
 mergeSort(arr, 0, arr.length-1);
-
+mergeHolder.push(arr.toString());
 
 console.log("Array after sort " , arr.toString());
 
@@ -136,147 +136,14 @@ function setup() {
 }
 
 function draw() {
+    background(255);
 
-    
-    
+    //moved all the code which was previously in the draw function
+    //to setVal() just to make it eaiser to use draw()
+
+    noLoop();
 }
 
-
-// // Takes in an array, merges two sub arrays from it 
-// // first sub array is passedArray[l ... m]
-// // second sub array is passedArray [m+1 ... r]
-// function merge(passedArray, l, m, r) {
-    
-    
-//     var i, j, k; 
-//     var n1 = m - l + 1; 
-//     var n2 = r - m;
-
-
-//     // create temporary arrays 
-//     var LeftArray = [];
-//     var RightArray = [];
-
-//     for (var v = 0; v < n1; v++ ) {
-//         LeftArray.push(0);
-//     }
-
-//     for (var x = 0; x < n2; x++ ) {
-//         RightArray.push(0);
-//     }
-    
-
-//     // copy data to temporary arrays left and right 
-//     for (i = 0; i < n1; i++) {
-//         LeftArray[i] = passedArray[l + i];
-//     }
-
-//     for (j = 0; j < n2; j++) {
-//         RightArray[j] = arr[m + 1 + j];
-//     }
-
-//     // merge the temporary arrays back into passed Array 
-//     i = 0; // initial index of first subarray
-//     j = 0; // initial index of second subarray
-//     k = l; // initial index of merged subarray
-
-//     while (i < n1 && j < n2) {
-//         if (LeftArray[i] <= RightArray[j]) {
-//             passedArray[k] = LeftArray[i];
-//             i++;
-//         }
-//         else {
-//             passedArray[k] = RightArray[j];
-//             j++;
-//         }
-//         k++;
-//     }
-
-//     //copy the remaining elements of Left Array, if there are any 
-//     while (i < n1) {
-//         passedArray[k] = LeftArray[i];
-//         i++; 
-//         k++;
-//     }
-
-//     //copy the remaining elements of Right Array, if there are any 
-//     while (j < n2) {
-//         passedArray[k] = RightArray[j];
-//         j++;
-//         k++;
-//     }
-//     // let resultArray = [], leftIndex = 0, rightIndex = 0;
-
-//     // // We will concatenate values into the resultArray in order
-   
-//     //     if (left[leftIndex] < right[rightIndex]) {
-//     //         resultArray.push(left[leftIndex]);
-//     //         leftIndex++; // move left array cursor
-//     //     } else {
-//     //         resultArray.push(right[rightIndex]);
-//     //         rightIndex++; // move right array cursor
-//     //     }
-
-//     // for (i = 0; i < resultArray.length; i++) {
-//     //     //print(resultArray[i]);
-//     //     //console.log(resultArray[i]);
-//     //     console.log("In merge, step, ", i, " are at ", resultArray[i], "array size: ", resultArray.length);
-        
-//     // }
-//     // console.log("Finished merge, " , resultArray.toString());
-//     // // We need to concat to the resultArray because there will be one element left over after the while loop
-//     // return resultArray
-//     //     .concat(left.slice(leftIndex))
-//     //     .concat(right.slice(rightIndex));
-// }
-
-
-// // passedArray is the array to be sorted
-// // l is for left index of subarray of passedArray
-// // r is for right index of subarray of passedArray 
-// function mergeSort(passedArray, l, r) {
-//     // No need to sort the array if the array only has one element or empty
-//    if (l < r) {
-//        var m = Math.floor(1 + (r - 1) / 2);
-//        console.log("l is ", l, "r is " , r, "middle is " , m);
-
-//         mergeSort(passedArray, l, m);
-//         mergeSort(passedArray, m+1, r);
-
-//         merge(passedArray, l, m, r);
-//    }
-    
-//     // if (unsortedArray.length <= 1) {
-//     //     return unsortedArray;
-//     // }
-//     // // In order to divide the array in half, we need to figure out the middle
-//     // const middle = Math.floor(unsortedArray.length / 2);
-
-//     // // This is where we will be dividing the array into left and right
-//     // const left = unsortedArray.slice(0, middle);
-//     // const right = unsortedArray.slice(middle);
-
-//     // // Using recursion to combine the left and right
-//     // return merge(
-//     //     mergeSort(left), mergeSort(right)
-//     // );
-// }
-
-
-
-// function printArray (passedArray , size ) {
-//     var i; 
-
-//     for ( i = 0; i < size ; i++ ) {
-//         console.log(passedArray[i].toString());
-//     }
-
-// }
-
-// var arraySize = arr.length;
-// console.log("Before merge sort : ", arr.toString());
-// mergeSort(arr, 0, arraySize - 1);
-// console.log("After merge sort : " , arr.toString())
 
 function QuickSort(arr, left = 0, right = arr.length - 1) {
     let len = arr.length,
