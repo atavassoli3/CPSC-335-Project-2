976428614fd7f1ea0b06d8499a59d3e3104e024f
Vincent DeAugustine <32307897+vdeaugustine@users.noreply.github.com>
2020-03-27 17:03:46 -0700

    added Poresort

--- Script1.js
+++ Script1.js
@@ -1,6 +1,18 @@
-﻿
-var arr = ['0', 5, 'A', 6, 2, 7, 'B', 2, 'B', 6, 0, 3];
+console.log("Console Test");
+for (i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+
+var arr = ['0', 5, 'A', 6, 2, 7, 'B', 2, 'B' , 6, 0, 3];
 QuickSort(arr);
+mergeSort(arr);
+
+
+
+
+
+
 
 
 function setup() {
@@ -14,8 +26,6 @@ function draw() {
     
     
 }
-
-
 
 
 function mergeSort(unsortedArray) {
@@ -50,19 +60,18 @@ function merge(left, right) {
             rightIndex++; // move right array cursor
         }
 
-
     for (i = 0; i < resultArray.length; i++) {
-        print(resultArray[i]);
+        //print(resultArray[i]);
+        //console.log(resultArray[i]);
+        console.log("In merge, step, ", i, " are at ", resultArray[i], "array size: ", resultArray.length);
+        
     }
-
+    console.log("Finished merge, " , resultArray.toString());
     // We need to concat to the resultArray because there will be one element left over after the while loop
     return resultArray
         .concat(left.slice(leftIndex))
         .concat(right.slice(rightIndex));
 }
-
-
-
 
 
 function QuickSort(arr, left = 0, right = arr.length - 1) {
@@ -83,10 +92,15 @@ function QuickSort(arr, left = 0, right = arr.length - 1) {
 
     }
 
-
+    
     for (i = 0; i < arr.length; i++) {
-        print(arr[i]);
+        //print(arr[i]);
+        //console.log(arr[i]);
+        console.log("In quick, are at ", arr[i]);
+
     }
+    console.log("Finished quick, " , arr.toString());
+
 
     return arr
 
@@ -115,9 +129,12 @@ function partition(arr, left, right) {
         // If the left pointer is less than or equal to the 
         // right pointer, then swap values
         if (i <= j) {
+            console.log(i, " is less than or equal to ", j, " so we are swapping.");
+            console.log("Array before: ", arr.toString());
             [arr[i], arr[j]] = [arr[j], arr[i]]  // ES6 destructuring swap
             i++
             j--
+            console.log("Array after: ", arr.toString());
         }
     }
 
@@ -125,29 +142,29 @@ function partition(arr, left, right) {
 
 }
 
+function poresort1pass(a) {
 
-
-function poresort(a) {
-  
-    var n = (a.length-1)/2;
+    var n = a.length - 1;
     var x = a;
-   
-      //checks even (0,2,4,6...)
-        for (var i = 0; i < n; i+=2) {
-            if (x[i] < x[i + 1]) {
-                var temp = x[i];
-                x[i] = x[i + 1];
-                x[i + 1] = temp;
-            }
+
+    //checks even (0,2,4,6...)
+    for (var i = 0; i < n; i += 2) {
+        if (x[i] < x[i + 1]) {
+            var temp = x[i];
+            x[i] = x[i + 1];
+            x[i + 1] = temp;
+        } 
+
+    }
 
         //checks odd (1,3,5,7...)
-        for (var i = 1; i < n; i+=2) {
+        for (var i = 1; i < n; i += 2) {
             if (x[i] < x[i + 1]) {
                 var temp = x[i];
                 x[i] = x[i + 1];
                 x[i + 1] = temp;
             }
         }
-   
-    return x;
-}
+
+        return x;
+  }
