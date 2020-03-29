bb7c5ddf772279d493e90d52d13af4b43b75252b
thqmasinsomnia <tsmith142@saddleback.edu>
2020-03-28 20:13:46 -0700

    yuh

--- Script1.js
+++ Script1.js
@@ -1,170 +1,193 @@
-console.log("Console Test");
-for (i = 0; i < 10; i++) {
-    console.log(i);
-}
 
 
-var arr = ['0', 5, 'A', 6, 2, 7, 'B', 2, 'B' , 6, 0, 3];
-QuickSort(arr);
-mergeSort(arr);
-
-
-
-
-
-
-
+var goldHolder = [];
+var quickHolder = [];
+var mergeHolder = [];
 
 function setup() {
-
-    createCanvas(1604, 1604);
-
+    createCanvas(800, 800);
 }
 
 function draw() {
-
-    
-    
+    background(226);
+    text("GOLD PORESORT", 100, 100);
+    text("MERGESORT", 340, 100);
+    text("QUICKSORT", 540, 100);
 }
 
 
-function mergeSort(unsortedArray) {
-    // No need to sort the array if the array only has one element or empty
-    if (unsortedArray.length <= 1) {
-        return unsortedArray;
-    }
-    // In order to divide the array in half, we need to figure out the middle
-    const middle = Math.floor(unsortedArray.length / 2);
-
-    // This is where we will be dividing the array into left and right
-    const left = unsortedArray.slice(0, middle);
-    const right = unsortedArray.slice(middle);
-
-    // Using recursion to combine the left and right
-    return merge(
-        mergeSort(left), mergeSort(right)
-    );
-}
-
-// Merge the two arrays: left and right
-function merge(left, right) {
-    let resultArray = [], leftIndex = 0, rightIndex = 0;
-
-    // We will concatenate values into the resultArray in order
-   
-        if (left[leftIndex] < right[rightIndex]) {
-            resultArray.push(left[leftIndex]);
-            leftIndex++; // move left array cursor
-        } else {
-            resultArray.push(right[rightIndex]);
-            rightIndex++; // move right array cursor
-        }
-
-    for (i = 0; i < resultArray.length; i++) {
-        //print(resultArray[i]);
-        //console.log(resultArray[i]);
-        console.log("In merge, step, ", i, " are at ", resultArray[i], "array size: ", resultArray.length);
-        
-    }
-    console.log("Finished merge, " , resultArray.toString());
-    // We need to concat to the resultArray because there will be one element left over after the while loop
-    return resultArray
-        .concat(left.slice(leftIndex))
-        .concat(right.slice(rightIndex));
-}
 
 
-function QuickSort(arr, left = 0, right = arr.length - 1) {
-    let len = arr.length,
-        index
-
-    if (len > 1) {
-
-        index = partition(arr, left, right)
-
-        if (left < index - 1) {
-            QuickSort(arr, left, index - 1)
-        }
-
-        if (index < right) {
-            QuickSort(arr, index, right)
-        }
-
-    }
-
-    
-    for (i = 0; i < arr.length; i++) {
-        //print(arr[i]);
-        //console.log(arr[i]);
-        console.log("In quick, are at ", arr[i]);
-
-    }
-    console.log("Finished quick, " , arr.toString());
-
-
-    return arr
-
-}
-
-function partition(arr, left, right) {
-    let middle = Math.floor((right + left) / 2),
-        pivot = arr[middle],
-        i = left,                 // Start pointer at the first item in the array
-        j = right                 // Start pointer at the last item in the array
-
-    while (i <= j) {
-
-        // Move left pointer to the right until the value at the
-        // left is greater than the pivot value
-        while (arr[i] < pivot) {
-            i++
-        }
-
-        // Move right pointer to the left until the value at the
-        // right is less than the pivot value
-        while (arr[j] > pivot) {
-            j--
-        }
-
-        // If the left pointer is less than or equal to the 
-        // right pointer, then swap values
-        if (i <= j) {
-            console.log(i, " is less than or equal to ", j, " so we are swapping.");
-            console.log("Array before: ", arr.toString());
-            [arr[i], arr[j]] = [arr[j], arr[i]]  // ES6 destructuring swap
-            i++
-            j--
-            console.log("Array after: ", arr.toString());
-        }
-    }
-
-    return i
-
-}
-
-function poresort1pass(a) {
+function poresort(a) {
 
     var n = a.length - 1;
     var x = a;
+    var swap;
 
-    //checks even (0,2,4,6...)
-    for (var i = 0; i < n; i += 2) {
-        if (x[i] < x[i + 1]) {
-            var temp = x[i];
-            x[i] = x[i + 1];
-            x[i + 1] = temp;
-        } 
-
-    }
-
-        //checks odd (1,3,5,7...)
-        for (var i = 1; i < n; i += 2) {
-            if (x[i] < x[i + 1]) {
+    do {
+        swap = false;
+        //checks even (0,2,4,6...)
+        for (var i = 0; i < n; i += 2){
+           
+            if (x[i] > x[i + 1]) {
                 var temp = x[i];
                 x[i] = x[i + 1];
                 x[i + 1] = temp;
+                swap = true;
+                goldHolder.push(x.toString());
+
             }
+
         }
 
-        return x;
-  }
+        //checks odd (1,3,5,7...)
+        for (var i = 1; i < n; i += 2) {
+           
+            if (x[i] > x[i + 1]) {
+                var temp = x[i];
+                x[i] = x[i + 1];
+                x[i + 1] = temp;
+                swap = true;
+                goldHolder.push(x.toString());
+               
+            }
+        }
+    
+  
+    } while (swap)
+
+    return x;
+}
+
+
+function quickSort(items, left = 0, right = items.length - 1) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+         
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+
+        }
+    }
+
+    return items;
+}
+function swap(items, leftIndex, rightIndex) {
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+function partition(items, left, right) {
+
+    var pivot = items[Math.floor((right + left) / 2)], //middle element
+        i = left, //left pointer
+        j = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            quickHolder.push(items.toString());
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+
+
+
+function merge(arr, l, m, r) {
+
+    var i, j, k;
+    var n1 = m - l + 1;
+    var n2 = r - m;
+
+
+    /* create temp arrays */
+    var L = [];
+    var R = [];
+
+
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) {
+        L[i] = arr[l + i];
+    }
+    for (j = 0; j < n2; j++) {
+        R[j] = arr[m + 1 + j];
+    }
+
+  
+
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+    mergeHolder.push(arr.toString());
+}
+function mergeSort(arr, l = 0, r = arr.length - 1) {
+ 
+
+    // int arr_size = 12;
+    // printf("\nNew mergeSort call, printing array.");
+    // printArray(arr, arr_size);
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        var m = Math.floor(l + (r - l) / 2);
+
+        // Sort first and second halves 
+        mergeSort(arr, l, m);
+      
+        // printArray(arr, array_Size);
+        mergeSort(arr, m + 1, r);
+   
+        // printArray(arr, array_Size);
+        merge(arr, l, m, r);
+
+    }
+
+ 
+} 
+
+
